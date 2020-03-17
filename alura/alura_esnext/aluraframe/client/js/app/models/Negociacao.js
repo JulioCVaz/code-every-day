86131d6037a9e80e8727804eba08f0d4afb35429
Júlio Vaz <julio.oliveiravaz0@gmail.com>
2020-03-16 23:38:43 -0300

    tornando a instancia new Negociacao imutavel

--- alura/alura_esnext/aluraframe/client/js/app/models/Negociacao.js
+++ alura/alura_esnext/aluraframe/client/js/app/models/Negociacao.js
@@ -1,8 +1,26 @@
 class Negociacao {
-    constructor(){
-        
-        this.data = new Date();
-        this.quantidade = 1;
-        this.valor = 0.0;
+    constructor(data, quantidade, valor){
+
+        this._data = new Date(data.getTime());
+        this._quantidade = quantidade;
+        this._valor = valor;
+        Object.freeze(this);
+        // .freeze faz um shallow freeze da minha instancia, tornando as propriedades "imutaveis"
+    }
+
+    get volume() {
+        return this._quantidade * this._valor;
+    }
+
+    get data() {
+        return new Date(this._data.getTime());
+    }
+
+    get quantidade() {
+        return this._quantidade;
+    }
+
+    get valor() {
+        return this._valor;
     }
 }
